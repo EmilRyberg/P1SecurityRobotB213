feab214da606933e33281acee6d778945bbadc04
Unknown <mrth17@student.aau.dk>
2017-11-22 12:34:08 +0100

    Implemented a test code for the sound_play package, in order to lean it easier

--- src/audio_node/src/audio_node.cpp
+++ src/audio_node/src/audio_node.cpp
@@ -1,11 +1,78 @@
-#include <iostream>
-#include <ros/ros.h>
-#include "sound_play/sound_play.h"
+#include <sound_play/sound_play.h>
+#include <unistd.h>
 
-
-int main(int argc, char *argv[])
+//Believe this function is to check the amount of time passed
+void sleepok(int t, ros::NodeHandle &nh)
 {
-    ros::init(argc, argv, "audio_node");
+  if (nh.ok())
+      sleep(t);
+}
 
-    return 0;
+//start of main
+int main(int argc, char **argv)
+{
+ ros::init(argc, argv, "sound_play_test");
+
+ //giving handles to specific ros nodes to minize writing time
+ ros::-NodeHandle- nh;
+ sound_play::SoundClient sc;
+
+//calls the void, makes the robot be standby for 1 second
+ sleepok(1, nh);
+
+ while(nh.ok())
+   {
+     //Says hello world and then waits 2 seconds
+     sc.say("Hello world!");
+     sleepok(2, nh);
+
+     //Repeats the string for 4 seconds before it stops
+     const char *str1 = "I am annoying.";
+     sc.repeat(str1);
+     sleepok(4, nh);
+     sc.stopSaying(str1);
+
+     //plays a .wav file
+     sc.playWave("/usr/share/xemacs21/xemacs-packages/etc/sounds/boing.wav");
+     sleepok(2, nh);
+
+     //play a .wav and stops it after t seconds
+     const char *str2 = "/usr/share/xemacs21/xemacs-packages/etc/sounds/piano-beep.wav";
+     sc.startWave(str2);
+     sleepok(4, nh);
+     sc.stopWave(str2);
+
+     //plays sounds from the sound_play package
+     sc.play(sound_play::SoundRequest::NEEDS_UNPLUGGING);
+     sleepok(2, nh);
+
+     //plays sound from sound_play package for 4 seconds
+     sc.start(sound_play::SoundRequest::BACKINGUP);
+     sleepok(4, nh);
+     sc.stop(sound_play::SoundRequest::BACKINGUP);
+
+                 sleepok(2, nh);
+                 sound_play::Sound s1 = sc.waveSound("/usr/share/xemacs21/xemacs-packages/etc/sounds/boing.wav");
+                 s1.repeat();
+                 sleepok(1, nh);
+                 s1.stop();
+
+                 sleepok(2, nh);
+                 sound_play::Sound s2 = sc.voiceSound("This is a really long sentence that will get cut off.");
+                 s2.play();
+                 sleepok(1, nh);
+                 s2.stop();
+
+                 sleepok(2, nh);
+                 sound_play::Sound s3 = sc.builtinSound(sound_play::SoundRequest::NEEDS_UNPLUGGING_BADLY);
+                 s3.play();
+                 sleepok(1, nh);
+                 s3.stop();
+
+                 sleepok(2, nh);
+                 sound_play::Sound s4 = sc.waveSoundFromPkg("sound_play", "sounds/BACKINGUP.ogg");
+                 s4.play();
+                 sleepok(1, nh);
+                 s4.stop();
+   }
 }
