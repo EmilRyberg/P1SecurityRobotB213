e635c830ebfca48c01d3f95d158cbe9d20abe109
Mads <mrth17@student.aau.dk>
2017-11-27 15:54:45 +0100

    Made a proper subscriber hopefully. If it is still not correct, tell me i am a dumb autist that should kill myself.

--- src/audio_node/src/audio_node.cpp
+++ src/audio_node/src/audio_node.cpp
@@ -9,33 +9,28 @@ void sleepok(int t, ros::NodeHandle &nh)
       sleep(t);
 }
 
+void chatterCallback(const std_msgs::Byte::ConstPtr& msg){
+  if (msg==1) {
+    sc.say("You have proper clearance, move along citizen");
+  } else {
+    sc.say("Intruder Detected, A higher authority will be here soon");
+    sleepok(6, nh);
+    sc.playWave("Siren_Noise-KevanGC-1337458893.wav");
+  }
+}
+
 //start of main
 int main(int argc, char **argv)
 {
  ros::init(argc, argv, "sound_play_test");
-
-
+ 
  //giving handles to specific ros nodes to minize writing time
  ros::-NodeHandle- nh;
  sound_play::SoundClient sc;
 
- ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
- std_msgs::Byte message;
-
-
-// My actual code
-sleepok(1, nh);
-sc.say("Anomaly detected. Please show your QR code");
-sleepok(3, nh);
-
-if (/* QR code is correct */) {
-  sc.say("You have proper clearance, move along citizen");
-}
-else {
-sc.say("Intruder Detected, A higher authority will be here soon");
-sleepok(6, nh);
-sc.playWave("Siren_Noise-KevanGC-1337458893.wav");
-}
+ //subscribes to a node
+ ros::Subscriber sub = n.subscribe("audio/play_sound", 1000, chatterCallback);
+ ros::spin();
 
 return 0;
 }
