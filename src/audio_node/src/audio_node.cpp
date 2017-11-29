bd20f849ab17df20118ecbe8eddfe29130073985
Mads <mrth17@student.aau.dk>
2017-11-29 15:51:08 +0100

    It is now built, don't know if the node actually works

--- src/audio_node/src/audio_node.cpp
+++ src/audio_node/src/audio_node.cpp
@@ -1,36 +1,43 @@
-#include <sound_play/sound_play.h>
+#include </opt/ros/kinetic/include/sound_play/sound_play.h>
 #include <unistd.h>
-#include "std_msgs/Byte.h"
-#include "ros/ros.h"
+#include "/opt/ros/kinetic/include/std_msgs/Byte.h"
+#include "/opt/ros/kinetic/include/ros/ros.h"
 
-//Believe this function is to check the amount of time passed
+ros::NodeHandle *nh_ptr;
+sound_play::SoundClient *sc_ptr;
+
+//Function is to check the amount of time passed
 void sleepok(int t, ros::NodeHandle &nh)
 {
-  if (nh.ok())
+  if (nh_ptr->ok())
       sleep(t);
 }
 
+//Function to play sounds according to the message recieved
 void chatterCallback(const std_msgs::Byte::ConstPtr& msg){
-  if (msg==1) {
-    sc.say("You have proper clearance, move along citizen");
+  int my_counter=msg->data;
+  if (my_counter==1) {
+    sc_ptr->say("You have proper clearance, move along citizen");
   } else {
-    sc.say("Intruder Detected, A higher authority will be here soon");
-    sleepok(6, nh);
-    sc.playWave("Siren_Noise-KevanGC-1337458893.wav");
+    sc_ptr->say("Intruder Detected, A higher authority will be here soon");
+    sleepok(6, *nh_ptr);
+    sc_ptr->playWave("Siren_Noise-KevanGC-1337458893.wav");
   }
 }
 
-//start of main
+  //start of main
 int main(int argc, char **argv)
 {
  ros::init(argc, argv, "audio_node");
 
  //giving handles to specific ros nodes to minize writing time
  ros::NodeHandle nh;
+ nh_ptr = &nh;
  sound_play::SoundClient sc;
+ sc_ptr = &sc;
 
  //subscribes to a node
- ros::Subscriber sub = n.subscribe("audio/play_sound", 1000, chatterCallback);
+ ros::Subscriber sub = nh.subscribe("audio/play_sound", 1000, chatterCallback);
  ros::spin();
 
 return 0;
