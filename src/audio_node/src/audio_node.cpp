3dab8a2aff81a9474d095d57d0cfc3fd124f8cf1
Mads <mrth17@student.aau.dk>
2017-11-30 12:58:32 +0100

    Added comments

--- src/audio_node/src/audio_node.cpp
+++ src/audio_node/src/audio_node.cpp
@@ -6,7 +6,7 @@
 ros::NodeHandle *nh_ptr;
 sound_play::SoundClient *sc_ptr;
 
-//Function is to check the amount of time passed
+//Function is to pass an amount of time
 void sleepok(int t, ros::NodeHandle &nh)
 {
   if (nh_ptr->ok())
@@ -16,10 +16,12 @@ void sleepok(int t, ros::NodeHandle &nh)
 //Function to play sounds according to the message recieved
 void chatterCallback(const std_msgs::Byte::ConstPtr& msg){
 int my_counter=msg->data;
+//Acts according to the msg recieved
   if (my_counter==1) {
     sc_ptr->say("You have proper clearance, move along citizen");
   } else {
     sc_ptr->say("Intruder Detected, A higher authority will be here soon");
+    // waits 5 seconds via sleepok function
     sleepok(5, *nh_ptr);
     sc_ptr->playWave("/opt/ros/kinetic/share/sound_play/sounds/Siren_Noise-KevanGC-1337458893.wav");
   }
@@ -36,8 +38,7 @@ int main(int argc, char **argv)
  sound_play::SoundClient sc;
  sc_ptr = &sc;
 
-
- //subscribes to a node
+ //subscribes to a node (audio_play_sound)
  ros::Subscriber sub = nh.subscribe("audio/play_sound", 1000, chatterCallback);
  ros::spin();
 
