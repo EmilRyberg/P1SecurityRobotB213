746339c8753cf05595cf0e8b24f382fcea8f27c9
Unknown <eryber17@student.aau.dk>
2017-11-30 11:07:21 +0100

    MORE HSV

--- src/image_recognition/src/color_detection_node.cpp
+++ src/image_recognition/src/color_detection_node.cpp
@@ -75,8 +75,8 @@ void ImageRawCallback(const sensor_msgs::ImageConstPtr &data_ptr)
 	Mat cv_image = cv_ptr->image;			 //makes a CvImage(Mat) format copy of the picture
 	Mat img_hsv;							 //Declares an empty picturefile of mat format
 	cvtColor(cv_image, img_hsv, CV_BGR2HSV); //Converts the image to grayscale and puts it into img_hsv
-	int low_h = 140; //sets the low hue threshold
-	int high_h = 160; //sets the high hue threshold 
+	int low_h = 130; //sets the low hue threshold
+	int high_h = 170; //sets the high hue threshold 
 	int low_s = 90; //sets the low saturation threshold
 	int high_s = 255; //sets the high saturation threshold
 	int low_v = 80; //sets the low value threshold
