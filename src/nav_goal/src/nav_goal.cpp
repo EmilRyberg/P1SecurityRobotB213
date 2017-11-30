551f3d502565340c8689b33f15eb6205511cdc97
Unknown <bgerga17@student.aau.dk>
2017-11-30 11:56:08 +0100

    initial navigation node

--- src/nav_goal/src/nav_goal.cpp
+++ src/nav_goal/src/nav_goal.cpp
@@ -1,11 +1,22 @@
 #include "ros/ros.h"
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
+#include "nav_goal/navigation_goal.h"
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
+void navigationCallback(const nav_goal::navigation_goal::ConstPtr& msg){
+  ROS_INFO("input",msg->goal_x);
+}
+
 int main(int argc, char** argv){
-  ros::init(argc, argv, "simple_navigation_goals");
+  ros::init(argc, argv, "navigation_translator");
+
+  ros::NodeHandle n;
+
+  ros::Subscriber sub = n.subscribe("navigation_goal",1000,navigationCallback);
+
+  ros::spin();
 
   //tell the action client that we want to spin a thread by default
   MoveBaseClient ac("move_base", true);
@@ -18,17 +29,45 @@ int main(int argc, char** argv){
   move_base_msgs::MoveBaseGoal goal;
 
   //we'll send a goal to the robot to move 1 meter forward
-  goal.target_pose.header.frame_id = "base_link";
+  goal.target_pose.header.frame_id = "/map";
   goal.target_pose.header.stamp = ros::Time::now();
-
-  goal.target_pose.pose.position.x = 3.0;
-  goal.target_pose.pose.orientation.w = 1.0;
+while(1){
+  goal.target_pose.pose.position.x = -0.43;
+  goal.target_pose.pose.position.y = 5.77;
+  goal.target_pose.pose.orientation.w = 3.0;
 
   ROS_INFO("Sending goal");
   ac.sendGoal(goal);
 
   ac.waitForResult();
 
+  goal.target_pose.pose.position.x = 1.34;
+  goal.target_pose.pose.position.y = 5.65;
+  goal.target_pose.pose.orientation.w = 3.0;
+
+  ROS_INFO("Sending goal");
+  ac.sendGoal(goal);
+
+  ac.waitForResult();
+
+  goal.target_pose.pose.position.x = 0.59;
+  goal.target_pose.pose.position.y = -5.4;
+  goal.target_pose.pose.orientation.w = 3.0;
+
+  ROS_INFO("Sending goal");
+  ac.sendGoal(goal);
+
+  ac.waitForResult();
+
+  goal.target_pose.pose.position.x = 2.46;
+  goal.target_pose.pose.position.y = -7.43;
+  goal.target_pose.pose.orientation.w = 3.0;
+
+  ROS_INFO("Sending goal");
+  ac.sendGoal(goal);
+
+  ac.waitForResult();
+}
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
     ROS_INFO("Hooray, the base moved 1 meter forward");
   else
